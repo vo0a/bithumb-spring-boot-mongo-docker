5048b7738bc96512307d5c8316e239555bf67f0f
daromKim <vo0a195@gmail.com>
2021-08-23 20:12:53 +0900

    rename : Repository 이름 변경

--- src/main/java/com/romkudev/api/customer/CustomerApplication.java
+++ src/main/java/com/romkudev/api/customer/CustomerApplication.java
@@ -7,20 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication @RequiredArgsConstructor
 public class CustomerApplication implements CommandLineRunner {
-	private final CustomRespository customRespository;
+	private final CustomerRespository customerRespository;
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		customRespository.deleteAll();
-		customRespository.save(new Customer("1", "Alice", "Smith"));
-		customRespository.save(new Customer("2", "Bob", "Smith"));
-		customRespository.save(new Customer("3", "Tom", "Cruise"));
+		customerRespository.deleteAll();
+		customerRespository.save(new Customer("1", "Alice", "Smith"));
+		customerRespository.save(new Customer("2", "Bob", "Smith"));
+		customerRespository.save(new Customer("3", "Tom", "Cruise"));
 
 		System.out.println("고객 이름 전부 찾기");
-		for(Customer customer : customRespository.findAll()) {
+		for(Customer customer : customerRespository.findAll()) {
 			System.out.println(customer);
 		}
 		System.out.println("=== 종료 ===");
--- src/main/java/com/romkudev/api/customer/CustomerRespository.java
+++ src/main/java/com/romkudev/api/customer/CustomerRespository.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomRespository extends MongoRepository<Customer, String> {
+public interface CustomerRespository extends MongoRepository<Customer, String> {
 }
